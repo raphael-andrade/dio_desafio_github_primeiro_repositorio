a8b2c479017e5e2fa0edc2137d86c40ea8a277c4
Raphael Andrade <raphael-andrade@live.com>
2022-09-25 22:17:35 -0300

    Programa referente ao exercicio proposto da aula 1 entendendo metodos java

---
+++ exercicios_propostos/Entendendo-metodos-java/src/Main.java
@@ -0,0 +1,52 @@
+import java.util.Calendar;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Calendar calendario = Calendar.getInstance();
+        double hora = calendario.get(Calendar.HOUR_OF_DAY);
+        Scanner sc = new Scanner(System.in);
+        int valor1;
+        int valor2;
+
+        if(hora > 5 && hora < 12){
+            System.out.println("Bom dia");
+        }
+        else if (hora > 12 && hora < 18){
+            System.out.println("Boa tarde");
+        }
+        else {
+            System.out.println("Boa noite");
+        }
+        System.out.println("Digite primeiro valor ");
+        valor1 = sc.nextInt();
+        System.out.println("Digite segundo valor");
+        valor2 = sc.nextInt();
+        double soma = valor1 + valor2;
+        double subtracao = valor1 - valor2;
+        double multiplicacao = valor1 * valor2;
+        double divisao = valor1 / valor2;
+
+        System.out.println("Soma = " + soma);
+        System.out.println("Subtação = " + subtracao);
+        System.out.println("Multiplicação = " + multiplicacao);
+        System.out.println("Divisão = " + divisao);
+
+        System.out.println("Emprestimo");
+        System.out.println("Valor do emprestimo: ");
+        double valorEmprestimo = sc.nextDouble();
+
+        System.out.println("Numero de parcelas: ");
+        int numeroParcelas = sc.nextInt();
+
+        System.out.println("Taxa de juros");
+        double juros = sc.nextDouble()/100;
+
+        double valorFinal = valorEmprestimo;
+        for (int i=0; i<=numeroParcelas; i++){
+        valorFinal+= valorEmprestimo * juros;
+         }
+        System.out.printf("Valor final R$" + "%.2f",valorFinal);
+    }
+
+    }
